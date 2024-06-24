d65392bc41f23471c7a3ca4464b232003955871d
Paul Jenkins <paul.jenkins@blackdice.io>
2024-06-24 15:18:53 +0100

    supress errors from missing files or directories on delete

--- tf/cleanup.sh
+++ tf/cleanup.sh
@@ -1,4 +1,4 @@
-rm -r .terraform
-rm .terraform.lock.hcl
-rm terraform.tfstate
-rm terraform.tfstate.backup
+rm -Rf .terraform 2> /dev/null
+rm .terraform.lock.hcl 2> /dev/null
+rm terraform.tfstate 2> /dev/null
+rm terraform.tfstate.backup 2> /dev/null
